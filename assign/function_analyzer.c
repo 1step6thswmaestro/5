6c9f50046d9d06594727d5c3f1fa61bca8c47fe2
SunKyu <leesunkeuy@naver.com>
2015-09-25 03:26:05 -0700

    function_analyzer

---
+++ assign/function_analyzer.c
@@ -0,0 +1,48 @@
+#include <uapi/linux/ptrace.h>
+
+struct fun_data {
+
+    int count;
+    u64 total_time;
+    u64 min_time;
+    u64 max_time;
+};
+
+BPF_HASH(stats, u32);
+BPF_TABLE("array", u32, struct fun_data, result, 8);
+
+int function_start(struct pt_regs *ctx){
+    u32 curr_pid = bpf_get_current_pid_tgid();
+    u64 time = bpf_ktime_get_ns();
+    stats.update(&curr_pid, &time);
+    return 0;
+
+}
+
+int function_exit(struct pt_regs *ctx){
+    u32 pid = bpf_get_current_pid_tgid();
+    u32 cpu = bpf_get_smp_processor_id();
+    u64 *val, delta;
+    val = stats.lookup(&pid);
+    if(val){
+        delta = bpf_ktime_get_ns() - *val;
+        struct fun_data *leaf = result.lookup(&cpu);
+        if (leaf){
+            leaf->count++;
+            leaf->total_time += delta;
+
+            if(leaf->min_time > delta)
+                leaf->min_time = delta;
+
+            if(leaf->max_time < delta)
+                leaf->max_time = delta;
+            result.update(&cpu,leaf);
+        }
+        else{
+            struct fun_data tp = {1, delta, delta, delta};
+            result.update(&cpu, &tp);
+        }   
+        stats.delete(&pid);
+    }
+    return 0;
+}
---
+++ assign/function_analyzer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+from bcc import BPF
+from time import sleep
+import sys
+
+b = BPF(src_file = "function_analyzer.c",debug = 6)
+
+func_name = "sys_clone"
+if len(sys.argv)>1:   
+    func_name = sys.argv[1]
+
+
+b.attach_kprobe(event = func_name, fn_name="function_start")
+
+b.attach_kretprobe(event = func_name, fn_name="function_exit")
+
+sleep(10)
+
+fun_data = {
+        "count" : 0,
+        "total_time" : 0,
+        "min_time" : None,
+        "max_time" : 0
+        }
+
+
+
+for k,v in b["result"].items():
+    fun_data["count"] += v.count
+    fun_data["total_time"] += v.total_time
+    if fun_data["min_time"] is None or fun_data["min_time"]> v.min_time:
+        fun_data["min_time"] = v.min_time
+
+    if fun_data["max_time"]< v.max_time:
+        fun_data["max_time"] = v.max_time
+
+
+for k, v in fun_data.items():
+    print (k, v)
+
+
+
+
+
+
+
+
+
+    
+
