ba976946bab40447ca9fcf1a4b5bf4f1917addae
SunKyu <leesunkeuy@naver.com>
2015-10-03 05:45:45 -0700

    for bug reporting

--- process/index_bug.c
+++ process/index_bug.c
@@ -1,20 +1,38 @@
 #include <uapi/linux/ptrace.h>
 
-BPF_TABLE("array", u64, u64, dist, 5);
+BPF_TABLE("array", int, int, dist, 5);
 
-int kprobe___do_fork (struct pt_regs *ctx, unsigned long flags){
+int index_sys_clone (struct pt_regs *ctx, unsigned long flags){
 
     int index =0;
-    u64  leaf =  0;
-    u64* val = dist.lookup_or_init(&index, &leaf);//0, 0
+    int zero = 0;
+    int  leaf =  0;
+    //u64* val = dist.lookup_or_init(&index, &leaf);//0, 0
+    //index++;leaf++;
+    int *val;
+    val = dist.lookup_or_init(&index, &zero);//0,0
+    (*val) = leaf;
     index++;leaf++;
-    val = dist.lookup_or_init(&index, &leaf);//1, 1
+
+    val = dist.lookup_or_init(&index, &zero);//1, 1
+    (*val) = leaf;
     index++;leaf++;
-    val = dist.lookup_or_init(&index,&leaf);//2, 2
+
+    val = dist.lookup_or_init(&index,&zero);//2, 2
+    (*val) = leaf;
     index++;leaf++;
-    val = dist.lookup_or_init(&index,&leaf);//3, 3
+
+    val = dist.lookup_or_init(&index,&zero);//3, 3
+    (*val) = leaf;
     index++;leaf++;
-    val = dist.lookup_or_init(&index,&leaf);//4, 4
+
+    val = dist.lookup_or_init(&index,&zero);//4, 4
+    (*val) = leaf;
+    index++;leaf++;
+
+    val = dist.lookup_or_init(&index,&zero);//5, 5
+    (*val) = leaf;
+    
     
     return 0;
 }
--- process/index_bug.py
+++ process/index_bug.py
@@ -2,13 +2,15 @@ from bcc import BPF
 from time import sleep
 
 
-b = BPF(src_file = "index_bug")
+b = BPF(src_file = "index_bug.c")
+b.attach_kprobe(event = "sys_clone", fn_name = "index_sys_clone")
 
 interval = 5
+do_exit = 0
 
 while 1:
     for k,v in b["dist"].items():
-        print (k.value, v.vlaue)
+        print (k.value, v.value)
     try:
         sleep(interval)
     except KeyboardInterrupt:
