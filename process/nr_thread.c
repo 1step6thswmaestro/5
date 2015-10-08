aa1a94592f782fe8e09703544a36efa7d5fecf41
SunKyu <leesunkeuy@naver.com>
2015-10-08 10:58:31 -0700

    make thread_count struct

--- process/nr_thread.c
+++ process/nr_thread.c
@@ -1,29 +1,31 @@
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 
-BPF_TABLE("array", int, int, dist, 4);
+struct thread_count{
+    int nr_curr;
+    int max;
+    int min;
+};
+
+BPF_TABLE("array", int, struct thread_count, dist, 1);
 
 int read_nr_thread(struct pt_regs *ctx, unsigned long flags){
 
     int n  = 0;
     bpf_probe_read(&n, sizeof(int), (void*) ADRVAR );
-    int index = 1;
-    int *nr_curr;
-    int *max;
-    int *min;
-    int zero = 0;
+    int index = 0;
+    struct thread_count * count_info;
+    struct thread_count  init_info = {0, 0, -1};
 
-    nr_curr = dist.lookup_or_init(&index, &zero);
-    (*nr_curr) = n;
-    index = 2;
-    max = dist.lookup_or_init(&index, &zero);
-    index = 3;
-    min = dist.lookup_or_init(&index, &zero);
-    if (n > (*max)){
-        (*max) = n;
+    count_info = dist.lookup_or_init(&index, &init_info);
+    count_info -> nr_curr = n;
+
+
+    if (n > count_info->max){
+        count_info->max = n;
     }
-    else if( n <(*min) || (*min)== 0){
-        (*min) = n;
+    else if( n <count_info->min || count_info->min== -1){
+        count_info->min = n;
     }
     
     return 0;
