34a07cded6280e61af77ffc301373b38c245283f
SunKyu <leesunkeuy@naver.com>
2015-10-11 09:51:09 -0700

    add max_allocate

--- memory/kmalloc.c
+++ memory/kmalloc.c
@@ -2,7 +2,8 @@
 #include <linux/slab.h>
 
 struct memory_info{
-    size_t allocate_size;
+    size_t total_allocate_size;
+    size_t max_allocate_size;
     size_t free_size;
 };
 
@@ -11,12 +12,12 @@ BPF_HASH(info, u32, struct memory_info);
 int kmal(struct pt_regs *ctx, size_t size){
     
     struct memory_info * leaf;
-    struct memory_info init;
-    init.allocate_size = 0;
-    init.free_size = 0;
+    struct memory_info init = {0, 0, 0};
     u32 pid =bpf_get_current_pid_tgid();
     leaf = info.lookup_or_init(&pid, &init);
-    leaf -> allocate_size += size;
+    leaf -> total_allocate_size += size;
+    if(size > leaf->max_allocate_size)
+        leaf->max_allocate_size = size;
     return 0;
 }
 /*
--- memory/kmalloc.py
+++ memory/kmalloc.py
@@ -4,7 +4,7 @@ from time import sleep
 b = BPF(src_file = "kmalloc.c")
 
 b.attach_kprobe(event = "__kmalloc", fn_name = "kmal")
-b.attach_kprobe(event = "__kfree", fn_name = "kfre")
+#b.attach_kprobe(event = "__kfree", fn_name = "kfre")
 
 
 
@@ -14,7 +14,7 @@ while 1:
 
     try:
         for k, v in b["info"].items():
-            print "pid : %s , allocate size %d, free size %d" %(k.value,v.allocate_size, v.free_size)
+            print "pid : %s, total allocate size %d, max allocate_size %d, free_size %d " %(k.value,v.total_allocate_size,v.max_allocate_size,  v.free_size)
     except KeyboardInterrupt:
         do_exit = 1
     
