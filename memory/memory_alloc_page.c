d95b858c2c29634f5a8535c646eef7718aaeb220
Dr.Z <korea.drzix@gmail.com>
2015-10-28 21:10:35 +0900

    memory_*_page: edit the parameter properly, edit the way to count the sum of siz using order

--- memory/memory_alloc_page.c
+++ memory/memory_alloc_page.c
@@ -19,9 +19,9 @@ struct memory_alloc_page_value
 BPF_TABLE("array", int, struct memory_alloc_page_value, memory_alloc_page_map, NUM_ARRAY_MAP_SIZE);
 
     // add memory_alloc_page_value.count one
-    // add memory_alloc_page_value.size allocated page size, 'order'
+    // add memory_alloc_page_value.size allocated page size, pow('order', 2)
     // when __alloc_pages_nodemask is called
-int memory_alloc_page_begin(struct pt_regs *ctx, unsigned int order)
+int memory_alloc_page_begin(struct pt_regs *ctx, gfp_t gfp_mask, unsigned int order)
 {
     struct memory_alloc_page_value *val, val_temp;
     int map_index = NUM_MAP_INDEX;
@@ -31,7 +31,7 @@ int memory_alloc_page_begin(struct pt_regs *ctx, unsigned int order)
     
     val = memory_alloc_page_map.lookup_or_init(&map_index, &val_temp);
     ++(val->count);
-    val->size += (u64)order;
+    val->size += 1 << (u64)order;
     
     cnt = val->count;
     siz = val->size;
--- memory/memory_free_page.c
+++ memory/memory_free_page.c
@@ -5,6 +5,7 @@
  */
 
 #include <uapi/linux/ptrace.h>
+#include <linux/pagevec.h>
 
 #define NUM_ARRAY_MAP_SIZE 1
 #define NUM_MAP_INDEX 0
@@ -19,9 +20,9 @@ struct memory_free_page_value
 BPF_TABLE("array", int, struct memory_free_page_value, memory_free_page_map, NUM_ARRAY_MAP_SIZE);
 
     // add memory_free_page_value.count one
-    // add memory_free_page_value.size freed page size, 'order'
-    // when _free_pages_ok is called
-int memory_free_page_begin(struct pt_regs *ctx, unsigned int order)
+    // add memory_free_page_value.size freed page size, pow('order', 2)
+    // when __free_pages_ok is called
+int memory_free_page_begin(struct pt_regs *ctx, struct page *page, unsigned int order)
 {
     struct memory_free_page_value *val, val_temp;
     int map_index = NUM_MAP_INDEX;
@@ -31,7 +32,7 @@ int memory_free_page_begin(struct pt_regs *ctx, unsigned int order)
     
     val = memory_free_page_map.lookup_or_init(&map_index, &val_temp);
     ++(val->count);
-    val->size += (u64)order;
+    val->size += 1 << (u64)order;
     
     cnt = val->count;
     siz = val->size;
@@ -51,7 +52,8 @@ int memory_free_page_order_zero_begin(struct pt_regs *ctx)
                         
     val = memory_free_page_map.lookup_or_init(&map_index, &val_temp);
     ++(val->count);
-    
+    ++(val_temp.size);
+
     cnt = val->count;
     siz = val->size;
     if (EXPRESSION)
