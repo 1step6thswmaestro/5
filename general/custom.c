8a8b52de4aadc634f6d2d0f635e1a8aeacd1f9b7
Dr.Z <korea.drzix@gmail.com>
2015-10-30 01:58:30 +0900

    add custom: it will count any selected function in /proc/kallsyms
    gonna get func name by regexp and use parameter 'event_re' in
    attach_kprobe()

---
+++ general/custom.c
@@ -0,0 +1,36 @@
+/*
+ * Event : custom.*
+ * Data to crawl : total count
+ * Used Kernel-function : Everything in /proc/kallsyms
+ */
+
+#include <uapi/linux/ptrace.h>
+
+#define NUM_ARRAY_MAP_SIZE 1
+#define NUM_MAP_INDEX 0
+
+struct custom_value
+{
+    u64 count;
+};
+
+    // map where we save total count
+BPF_TABLE("array", int, struct custom_value, custom_map, NUM_ARRAY_MAP_SIZE);
+
+    // add custom_value.count one when selected function is called
+int custom_begin(struct pt_regs *ctx)
+{
+    struct custom_value *val, val_temp;
+    int map_index = NUM_MAP_INDEX;
+    u64 cnt;
+    val_temp.count = 0;
+    
+    val = custom_map.lookup_or_init(&map_index, &val_temp);
+    ++(val->count);
+    
+    cnt = val->count;
+    if (EXPRESSION)
+        return 1;
+
+    return 0;
+}
