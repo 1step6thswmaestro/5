9be7b0e857d545828949164e7ddb41dc5e9b885e
SunKyu <leesunkeuy@naver.com>
2015-10-30 02:14:48 -0700

    remove part of  claculating size of writeback_bg
    we couldn't find the variable that had wirteback_bg's size
    so just removed

--- fs/fs_page_writeback_bg.c
+++ fs/fs_page_writeback_bg.c
@@ -13,7 +13,6 @@
 struct fs_page_writeback_bg_value
 {
     u64 count;
-    u64 size;
 };
 
 BPF_TABLE("array", int, struct fs_page_writeback_bg_value, fs_page_writeback_bg_map, NUM_ARRAY_MAP_SIZE);
@@ -21,17 +20,13 @@ BPF_TABLE("array", int, struct fs_page_writeback_bg_value, fs_page_writeback_bg_
 int fs_page_writeback_bg_begin(struct pt_regs *ctx, struct bdi_writeback * wb){
     struct fs_page_writeback_bg_value *val, val_temp;
     int map_index = NUM_MAP_INDEX;
-    u64 cnt, siz;
+    u64 cnt;
     val_temp.count = 0;
-    val_temp.size = 0;
 
     val = fs_page_writeback_bg_map.lookup_or_init(&map_index, &val_temp);
     ++(val->count);
-    //val->size +=
-    //need to find size variable in struct bdi_writeback *wb
 
     cnt = val->count;
-    siz = val->size;
     if(EXPRESSION)
         return 1;
 
