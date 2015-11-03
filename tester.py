a301739bee6a66ee28b491cd0af60c19a7c366fa
SunKyu <leesunkeuy@naver.com>
2015-11-02 17:08:56 -0800

    tester.py: change the interface to switchcase
    We made general.c file So we can use general source and replace
    some spcial word likes HEADER, PARAMETER and SIZE

--- tester.py
+++ tester.py
@@ -50,6 +50,75 @@ if len(sys.argv) == 1:
     print " "
     exit()
 
+def read_file(path):
+    with open(path, 'r') as f:
+        read_text = f.read()
+    return read_text
+
+def task_create():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ' ' )
+    source = source.replace("SIZE", '0')
+    return (source, "_do_fork")
+
+def task_exec():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ' ' )
+    source = source.replace("SIZE", '0')
+    return (source, "do_execveat_common.isra.34")
+
+def task_exit():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ' ' )
+    source = source.replace("SIZE", '0')
+    return (source, "do_exit")
+
+def task_switch():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ' ' )
+    source = source.replace("SIZE", '0')
+    return (source, "finish_task_switch")
+
+def memory_alloc():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ', size_t size' )
+    source = source.replace("SIZE", '(u64)size')
+    return (source, "__kmalloc")
+
+def memory_free():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", '' )
+    source = source.replace("SIZE", '0')
+    return (source, "kfree")
+
+def memory_alloc_page():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '')
+    source = source.replace("PARAMETER", ', gfp_t gfp_mask, unsigned int order' )
+    source = source.replace("SIZE", '1<<(u64)order')
+    return (source, "__alloc_pages_nodemask")
+
+def memory_free_page():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '#include <linux/pagevec.h>')
+    source = source.replace("PARAMETER", ', struct page *page, unsinged int order' )
+    source = source.replace("SIZE", '1 << (u64)order')
+    return (source, "__free_pages_ok")
+
+def memroy_reclaim_bc():
+    read_text = read_file("general/general.c")
+    source = read_text.replace("HEADER", '#include <linux/mmzone.h>\n#include<asm>page.h>')
+    source = source.replace("PARAMETER", ', pg_data_t *pgdat, int order')
+    source = source.replace("SIZE", '(2<<order) * PAGE_SIZE')
+    return (source, "balance_pgdat")
+
+
 EVENT_LIST = {
         "task.create" : ["task/task_create.c", "_do_fork", "task_create_begin", "task_create"],
         "task.exec" : ["task/task_exec.c", "do_execveat_common.isra.34", "task_exec_begin", "task_exec"],
@@ -69,24 +138,36 @@ EVENT_LIST = {
         "network.tcp_recv" : ["network/network_tcp_recv.c", "tcp_recvmsg", "network_tcp_recv_begin", "network_tcp_recv"],
         "network.udp_send" : ["network/network_udp_send.c", "udp_sendmsg", "network_udp_send_begin", "network_udp_send"],
         "network.udp_recv" : ["network/network_udp_recv.c", "udp_recvmsg", "network_udp_recv_begin", "network_udp_recv"],
-        "general.printk" : ["general/general_printk.c", "printk", "general_printk_begin", "general_printk"]
+        "general.printk" : ["general/general_printk.c", "printk", "general_printk_begin", "general_printk"],
+        "test.speed" : ["speedtest/task_switch.c", "finish_task_switch", "task_switch_begin", "task_switch"]
         }
 
-with open(EVENT_LIST[event][0], 'r') as f:
-    cfile = f.read()
+#with open(EVENT_LIST[event][0], 'r') as f:
+#    cfile = f.read()
+
 
 rep = "EXPRESSION"
+cfile = task_create()[0]
 bpf_code = cfile.replace(rep, expr)
 
 b = BPF(text = bpf_code, cb = call_back, debug=0)
-for i in range(0, multiprocessing.cpu_count()):
-    b.attach_kprobe(event = EVENT_LIST[event][1], fn_name = EVENT_LIST[event][2], cpu=i)
-    if event == "memory.free_page":
-        b.attach_kprobe(event = EVENT_LIST[event][4], fn_name = EVENT_LIST[event][5], cpu=i)
+b.attach_kprobe(event = task_create()[1], fn_name='func')
+
+#for i in range(0, multiprocessing.cpu_count()):
+#    b.attach_kprobe(event = EVENT_LIST[event][1], fn_name = EVENT_LIST[event][2], cpu=i)
+#    if event == "memory.free_page":
+#        b.attach_kprobe(event = EVENT_LIST[event][4], fn_name = EVENT_LIST[event][5], cpu=i)
 
 interval = interval * 1000
 b.kprobe_poll(timeout = interval)
 
-print_map()
+
+#if (event == "test.speed"):
+#    map_name = EVENT_LIST[event][3] + "_map"
+#    for k,v in b[map_name].items():
+#        print ("value : %d" %(v.count))
+
+
+#print_map()
 
 exit()
