a5aaa3403afd4f87b68faa902cd2d9a837dfb2f8
drzix <korea.drzix@gmail.com>
2015-11-11 02:21:14 +0900

    이벤트 매니저 멤버함수 최적화

--- include/evtmanage.py
+++ include/evtmanage.py
@@ -8,165 +8,154 @@ class EventManager:
     """
 
     def __init__(self):
+        self.source = self.read_file("../src/general.c")
         self.EVENT_LIST = {
-            "task.create": task_create(),
-            "task.exec": task_exec(),
-            "task.exit": task_exit(),
-            "task.switch": task_switch(),
-            "memory.alloc": memory_alloc(),
-            "memory.free": memory_free(),
-            "memory.alloc_page": memory_alloc_page(),
+            "task.create": self.task_create(),
+            "task.exec": self.task_exec(),
+            "task.exit": self.task_exit(),
+            "task.switch": self.task_switch(),
+            "memory.alloc": self.memory_alloc(),
+            "memory.free": self.memory_free(),
+            "memory.alloc_page": self.memory_alloc_page(),
             # "memory.free_page": ["memory/memory_free_page.c", "__free_pages_ok", "memory_free_page_begin", "memory_free_page", "free_hot_cold_page", "memory_free_page_order_zero_begin"],
-            "memory.reclaim": memroy_reclaim(),
-            "fs.pagecache_access": fs_pagecache_access(),
-            "fs.pagecache_miss": fs_pagecache_miss(),
-            "fs.read_ahead": fs_read_ahead(),
-            # "fs.page_writeback_bg": fs_page_writeback_bg(),
-            "fs.page_writeback_per_inode": fs_page_writeback_per_inode(),
-            "network.tcp_send": network_tcp_send(),
-            "network.tcp_recv": network_tcp_recv(),
-            "network.udp_send": network_udp_send(),
-            "network.udp_recv": network_udp_recv(),
+            "memory.reclaim": self.memroy_reclaim(),
+            "fs.pagecache_access": self.fs_pagecache_access(),
+            "fs.pagecache_miss": self.fs_pagecache_miss(),
+            "fs.read_ahead": self.fs_read_ahead(),
+            # "fs.page_writeback_bg": self.fs_page_writeback_bg(),
+            "fs.page_writeback_per_inode": self.fs_page_writeback_per_inode(),
+            "network.tcp_send": self.network_tcp_send(),
+            "network.tcp_recv": self.network_tcp_recv(),
+            "network.udp_send": self.network_udp_send(),
+            "network.udp_recv": self.network_udp_recv(),
         }
 
-    @staticmethod
-    def read_file(path):
+    def read_file(self, path):
         with open(path, 'r') as f:
-            read_text = f.read()
-        return read_text
+            return f.read()
 
-    @staticmethod
-    def task_exec():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "sys_execve"
+    def task_create(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "do_fork"
 
-    @staticmethod
-    def task_exit():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "do_exit"
+    def task_exec(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "sys_execve"
 
-    @staticmethod
-    def task_switch():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "finish_task_switch"
+    def task_exit(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "do_exit"
 
-    @staticmethod
-    def memory_alloc():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", ', size_t size')
-        source = source.replace("SIZE", '(u64)size')
-        return source, "__kmalloc"
+    def task_switch(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "finish_task_switch"
 
-    @staticmethod
-    def memory_free():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "kfree"
+    def memory_alloc(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", ', size_t size')\
+                   .replace("SIZE", '(u64)size'),\
+               "__kmalloc"
 
-    @staticmethod
-    def memory_alloc_page():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '#include<asm/page.h>')
-        source = source.replace("PARAMETER", ', gfp_t gfp_mask, unsigned int order')
-        source = source.replace("SIZE", '(1<<(u64)order)*PAGE_SIZE')
-        return source, "__alloc_pages_nodemask"
+    def memory_free(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "kfree"
 
-    @staticmethod
-    def memory_free_page():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '#include <linux/pagevec.h>\n#include<asm/page.h>')
-        source = source.replace("PARAMETER", ', struct page *page, unsinged int order')
-        source = source.replace("SIZE", '(1 << (u64)order) * PAGE_SIZE')
-        return source, "__free_pages_ok"
+    def memory_alloc_page(self):
+        return self.source\
+                   .replace("HEADER", '#include<asm/page.h>')\
+                   .replace("PARAMETER", ', gfp_t gfp_mask, unsigned int order')\
+                   .replace("SIZE", '(1<<(u64)order)*PAGE_SIZE'),\
+               "__alloc_pages_nodemask"
 
-    @staticmethod
-    def memroy_reclaim():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '#include <linux/mmzone.h>\n#include<asm/page.h>')
-        source = source.replace("PARAMETER", ', struct zonelist *zonelist, int order, gfp_t gfp_mask')
-        source = source.replace("SIZE", '(1<<order) * PAGE_SIZE')
-        return source, "try_to_free_pages"
+    def memory_free_page(self):
+        return self.source\
+                   .replace("HEADER", '#include <linux/pagevec.h>\n#include<asm/page.h>')\
+                   .replace("PARAMETER", ', struct page *page, unsinged int order')\
+                   .replace("SIZE", '(1 << (u64)order) * PAGE_SIZE'),\
+               "__free_pages_ok"
 
-    @staticmethod
-    def fs_pagecache_access():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "pagecache_get_page"
+    def memroy_reclaim(self):
+        return self.source\
+                   .replace("HEADER", '#include <linux/mmzone.h>\n#include<asm/page.h>')\
+                   .replace("PARAMETER", ', struct zonelist *zonelist, int order, gfp_t gfp_mask')\
+                   .replace("SIZE", '(1<<order) * PAGE_SIZE'),\
+               "try_to_free_pages"
 
-    @staticmethod
-    def fs_pagecache_miss():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", '')
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '0')
-        return source, "page_cache_sync_readahead"
+    def fs_pagecache_access(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "pagecache_get_page"
 
-    @staticmethod
-    def fs_read_ahead():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <linux/mm_types.h>\n#include<asm/page.h>")
-        source = source.replace("PARAMETER", '')
-        source = source.replace("SIZE", '(ctx->r8) * PAGE_SIZE')
-        return source, "__do_page_cache_readahead"
+    def fs_pagecache_miss(self):
+        return self.source\
+                   .replace("HEADER", '')\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '0'),\
+               "page_cache_sync_readahead"
 
-    @staticmethod
-    def fs_page_writeback_bg():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <linux/backing-dev-defs.h>")
-        source = source.replace("PARAMETER", ',struct bdi_writeback * wb')
-        source = source.replace("SIZE", '0')
-        return source, "wb_start_background_writeback"
+    def fs_read_ahead(self):
+        return self.source\
+                   .replace("HEADER", "#include <linux/mm_types.h>\n#include<asm/page.h>")\
+                   .replace("PARAMETER", '')\
+                   .replace("SIZE", '(ctx->r8) * PAGE_SIZE'),\
+               "__do_page_cache_readahead"
 
-    @staticmethod
-    def fs_page_writeback_per_inode():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <linux/writeback.h>")
-        source = source.replace("PARAMETER", ',struct inode *inode, struct writeback_control *wbc')
-        source = source.replace("SIZE", 'wbc->nr_to_write')
-        return source, "wb_start_background_writeback"
+    def fs_page_writeback_bg(self):
+        return self.source\
+                   .replace("HEADER", "#include <linux/backing-dev-defs.h>")\
+                   .replace("PARAMETER", ',struct bdi_writeback * wb')\
+                   .replace("SIZE", '0'),\
+               "wb_start_background_writeback"
 
-    @staticmethod
-    def network_tcp_recv():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <net/tcp.h>")
-        source = source.replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')
-        source = source.replace("SIZE", '(u64)len')
-        return source, "tcp_recvmsg"
+    def fs_page_writeback_per_inode(self):
+        return self.source\
+                   .replace("HEADER", "#include <linux/writeback.h>")\
+                   .replace("PARAMETER", ',struct inode *inode, struct writeback_control *wbc')\
+                   .replace("SIZE", 'wbc->nr_to_write'),\
+               "wb_start_background_writeback"
 
-    @staticmethod
-    def network_tcp_send():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <net/tcp.h>\n #include <net/inet_common.h>")
-        source = source.replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t size')
-        source = source.replace("SIZE", '(u64)size')
-        return source, "tcp_sendmsg"
+    def network_tcp_recv(self):
+        return self.source\
+                   .replace("HEADER", "#include <net/tcp.h>")\
+                   .replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')\
+                   .replace("SIZE", '(u64)len'),\
+               "tcp_recvmsg"
 
-    @staticmethod
-    def network_udp_send():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <net/udp.h>")
-        source = source.replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')
-        source = source.replace("SIZE", '(u64)len')
-        return source, "udp_sendmsg"
+    def network_tcp_send(self):
+        return self.source\
+                   .replace("HEADER", "#include <net/tcp.h>\n #include <net/inet_common.h>")\
+                   .replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t size')\
+                   .replace("SIZE", '(u64)size'),\
+               "tcp_sendmsg"
 
-    @staticmethod
-    def network_udp_recv():
-        read_text = read_file("general/general.c")
-        source = read_text.replace("HEADER", "#include <net/udp.h>")
-        source = source.replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')
-        source = source.replace("SIZE", '(u64)len')
-        return source, "udp_recvmsg"
+    def network_udp_send(self):
+        return self.source\
+                   .replace("HEADER", "#include <net/udp.h>")\
+                   .replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')\
+                   .replace("SIZE", '(u64)len'),\
+               "udp_sendmsg"
+
+    def network_udp_recv(self):
+        return self.source\
+                   .replace("HEADER", "#include <net/udp.h>")\
+                   .replace("PARAMETER", ',struct sock *sk, struct msghdr *msg, size_t len')\
+                   .replace("SIZE", '(u64)len'),\
+               "udp_recvmsg"
